04724d0ae7a3b966ce908d8c9b7cb25ee84f7951
oLeoBarreto <leobcontato@gmail.com>
2025-07-10 16:43:28 -0300

    feat: creating docker compose file to run project and add database connection

--- analytics-service/build.gradle.kts
+++ analytics-service/build.gradle.kts
@@ -24,6 +24,8 @@ dependencies {
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.config.yaml)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.postgresql)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
--- shortener-service/src/main/kotlin/com/leobarreto/shortener_service/Module/ShorterUrl/Domain/ShorterUrl.kt
+++ shortener-service/src/main/kotlin/com/leobarreto/shortener_service/Module/ShorterUrl/Domain/ShorterUrl.kt
@@ -1,3 +1,6 @@
 package com.leobarreto.shortener_service.Module.ShorterUrl.Domain
 
+import jakarta.persistence.Entity
+
+@Entity
 data class ShorterUrl(val originalUrl: String, val shortId: String) {}
