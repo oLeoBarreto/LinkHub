2cd42f97a31c06b9a8c94b09a54fdb407e12e06a
oLeoBarreto <leobcontato@gmail.com>
2025-09-16 15:20:17 -0300

    refactor(ShortenerService): implementing logs to services and try/catch to handle exceptions

--- shortener-service/src/main/kotlin/com/leobarreto/shortener_service/Module/ShorterUrl/Application/Services/CreateShorterUrlService.kt
+++ shortener-service/src/main/kotlin/com/leobarreto/shortener_service/Module/ShorterUrl/Application/Services/CreateShorterUrlService.kt
@@ -7,26 +7,41 @@ import com.leobarreto.shortener_service.Module.ShorterUrl.Infrastructure.Request
 import com.leobarreto.shortener_service.Module.ShorterUrl.Infrastructure.Response.CreateShorterUrlResponseDto
 import lombok.AllArgsConstructor
 import lombok.RequiredArgsConstructor
+import org.apache.coyote.BadRequestException
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 @RequiredArgsConstructor
 @AllArgsConstructor
 class CreateShorterUrlService(val repository: ShorterUrlRepository): ICreateShorterUrlContract {
+    private val log = LoggerFactory.getLogger(CreateShorterUrlService::class.java);
+
     override fun CreateNewShorterUrl(data: CreateShorterUrlDto, shortedByIP: String): CreateShorterUrlResponseDto {
-        val alphabet: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
-        val generatedId: String = generateRandomString(8,alphabet);
+        try {
+            log.debug("Starting to generate short url identification.");
 
-        val shorterUrl = ShorterUrl(generatedId, data.originalUrl);
-        shorterUrl.shortedByIP = shortedByIP;
-        repository.save(shorterUrl);
+            val alphabet: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+            val generatedId: String = generateRandomString(8,alphabet);
 
-        return CreateShorterUrlResponseDto(generatedId);
+            log.debug("Short identification generated.");
+
+            val shorterUrl = ShorterUrl(generatedId, data.originalUrl);
+            shorterUrl.shortedByIP = shortedByIP;
+            repository.save(shorterUrl);
+
+            log.info("Shorter URL generated and saved!");
+
+            return CreateShorterUrlResponseDto(generatedId);
+        } catch (ex: Exception) {
+            log.error("Error to create a short url.");
+            throw BadRequestException(ex.message);
+        }
     }
 
     private fun generateRandomString(length: Int, charSet: List<Char>): String {
-        val random = kotlin.random.Random.Default
-        return List(length) { charSet.random(random) }.joinToString("")
+        val random = kotlin.random.Random.Default;
+        return List(length) { charSet.random(random) }.joinToString("");
     }
 
 }
--- shortener-service/src/main/kotlin/com/leobarreto/shortener_service/Module/ShorterUrl/Application/Services/FindOriginalUrlService.kt
+++ shortener-service/src/main/kotlin/com/leobarreto/shortener_service/Module/ShorterUrl/Application/Services/FindOriginalUrlService.kt
@@ -5,15 +5,28 @@ import com.leobarreto.shortener_service.Module.ShorterUrl.Infrastructure.Databas
 import com.leobarreto.shortener_service.Module.ShorterUrl.Infrastructure.Response.FindOriginalUrlResponseDto
 import lombok.AllArgsConstructor
 import lombok.RequiredArgsConstructor
+import org.apache.coyote.BadRequestException
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 @RequiredArgsConstructor
 @AllArgsConstructor
 class FindOriginalUrlService(val repository: ShorterUrlRepository): IFindOriginalUrlContract {
-    override fun findOriginalUrl(shortId: String): FindOriginalUrlResponseDto {
-        val shorterUrl = repository.findById(shortId).orElseThrow();
+    private val log = LoggerFactory.getLogger(FindOriginalUrlService::class.java);
 
-        return FindOriginalUrlResponseDto(shorterUrl.originalUrl);
+    override fun findOriginalUrl(shortId: String): FindOriginalUrlResponseDto {
+        try {
+            log.debug("Starting to find original url.");
+
+            val shorterUrl = repository.findById(shortId).orElseThrow();
+
+            log.info("Original URL found with success!");
+
+            return FindOriginalUrlResponseDto(shorterUrl.originalUrl);
+        } catch (ex: Exception) {
+            log.error("Error to found original url");
+            throw BadRequestException(ex.message);
+        }
     }
 }
