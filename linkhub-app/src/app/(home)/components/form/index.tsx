a4b392f13c0b2ac57ea3a2b62762465a1b9547b1
oLeoBarreto <leobcontato@gmail.com>
2025-10-12 18:50:15 -0300

    feat(LinkHubApp): implementing redirect page and shorter url vizualization

--- linkhub-app/src/app/(home)/components/form/index.tsx
+++ linkhub-app/src/app/(home)/components/form/index.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { CreateShortUrl } from "@/actions/createShortUlr";
-import { FormEvent } from "react";
+import Link from "next/link";
+import { FormEvent, useState } from "react";
 import z from "zod";
 
 const schema = z.object({
@@ -9,6 +10,8 @@ const schema = z.object({
 });
 
 export function ShoterUrlForm() {
+  const [shortId, setShortId] = useState("");
+
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
@@ -19,29 +22,54 @@ export function ShoterUrlForm() {
     });
 
     if (validateSchema.success) {
-      await CreateShortUrl({ originalUrl: validateSchema.data.originalUrl });
+      const response = await CreateShortUrl({
+        originalUrl: validateSchema.data.originalUrl,
+      });
+
+      if (response != undefined) setShortId(response);
     }
   };
 
   return (
-    <form className="max-w-3xl mx-auto" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        id="url"
-        name="url"
-        aria-describedby="helper-text-explanation"
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#45caff] focus:border-[#45caff] block w-full h-12 p-2.5"
-        placeholder="Enter your link here"
-      />
-      <p id="helper-text-explanation" className="mt-2 text-sm text-gray-500">
-        Type your url here to get short version.
-      </p>
-      <button
-        type="submit"
-        className="w-full mt-5 text-white bg-gradient-to-t from-[#45caff] to-[#eca0ff] hover:bg-gradient-to-b hover:cursor-pointer focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-3"
-      >
-        Short URL
-      </button>
-    </form>
+    <div>
+      <form className="max-w-3xl mx-auto" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="url"
+          name="url"
+          aria-describedby="helper-text-explanation"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#45caff] focus:border-[#45caff] block w-full h-12 p-2.5"
+          placeholder="Enter your link here"
+        />
+        <p id="helper-text-explanation" className="mt-2 text-sm text-gray-500">
+          Type your url here to get short version.
+        </p>
+        <button
+          type="submit"
+          className="w-full mt-5 text-white bg-gradient-to-t from-[#45caff] to-[#eca0ff] hover:bg-gradient-to-b hover:cursor-pointer focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-3"
+        >
+          Short URL
+        </button>
+      </form>
+      {shortId && (
+        <div className="text-center mt-8">
+          <hr className="text-gray-300" />
+          <h2 className="text-xl font-semibold mt-2 mb-2">
+            URL shortened successfully!
+          </h2>
+
+          <Link
+            href={`/${shortId}`}
+            className="text-blue-500 hover:underline break-all"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {`${
+              typeof window !== "undefined" ? window.location.origin : ""
+            }/${shortId}`}
+          </Link>
+        </div>
+      )}
+    </div>
   );
 }
--- linkhub-app/src/lib/api.ts
+++ linkhub-app/src/lib/api.ts
@@ -2,5 +2,8 @@ import axios from "axios";
 
 export const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-    timeout: 1000
+    timeout: 1000,
+    validateStatus: (status: number): boolean => {
+        return status >= 200 && status < 400;
+    }
 })
