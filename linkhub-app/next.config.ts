9527022b0c1c3bf7649a4b7f65216515ed39d1df
oLeoBarreto <leobcontato@gmail.com>
2025-10-13 20:54:47 -0300

    build(LinkhubApp): adding dockerfile to build project image, and preparing to build version 1.0.0

--- linkhub-app/next.config.ts
+++ linkhub-app/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  crossOrigin: "anonymous"
+  crossOrigin: "anonymous",
+  output: "standalone"
 };
 
 export default nextConfig;
